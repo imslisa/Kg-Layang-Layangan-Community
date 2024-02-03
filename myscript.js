8417691ab04ba9a1f26257e6d0a6625ae87114ef
imslisa <154048605+imslisa@users.noreply.github.com>
2024-02-04 02:27:56 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,57 @@
+AOS.init();
+
+//header
+
+// slider images array
+
+const sliderImgs = ["mainbg.jpg", "img2.jpeg", "img3.jpeg", "img4.jpeg", "img5.jpeg", "img6.jpeg"];
+let sliderImage = document.querySelector('.background-image');
+let sliderGrids = [...document.querySelectorAll('.grid-item')];
+
+let currentImage = 0;
+
+setInterval(() => {
+    changeSliderImage();
+}, 5000);
+
+const changeSliderImage = () => {
+    sliderGrids.map((gridItem, index) => {
+        setTimeout(() => {
+            gridItem.classList.remove('hide');
+
+            setTimeout(() => {
+
+                if(index == sliderGrids.length - 1){
+                    if(currentImage >= sliderImgs.length - 1){
+                        currentImage = 0;
+                    } else{
+                        currentImage++;
+                    }
+
+                    sliderImage.src = `img/${sliderImgs[currentImage]}`;
+
+                    sliderGrids.map((item, i) => {
+                        setTimeout(() => {
+                            item.classList.add('hide')
+                        }, i * 100);
+                    })
+
+                }
+
+            }, 100);
+
+        }, index * 100);
+    })
+}
+
+// nav
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >= 188){
+        navbar.classList.add('bg');
+    } else{
+        navbar.classList.remove('bg')
+    }
+})
